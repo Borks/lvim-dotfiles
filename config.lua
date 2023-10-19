54bc5529d8ad41faacd4587518e2aea1a58c7f12
Borka Martin Orlov <borka@codemade.ee>
2023-10-19 17:14:42 +0300

    Initial commit

---
+++ config.lua
@@ -0,0 +1,16 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+lvim.keys.normal_mode["<C-d>"] = "<C-d>zz"
+lvim.keys.normal_mode["<C-u>"] = "<C-u>zz"
+lvim.transparent_window = true
+lvim.keys.normal_mode["gt"] = ":BufferLineCycleNext<CR>"
+lvim.keys.normal_mode["gT"] = ":BufferLineCyclePrev<CR>"
+vim.opt.relativenumber = true
+
+
+lvim.plugins = {
+  {"posva/vim-vue"},
+  {"iloginow/vim-stylus"}
+}
